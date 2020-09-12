cf07787fb76a3b2ec4e1ec08b45cc6177a88dbbf
homi39 <misa.horiuchi0302@gmail.com>
2020-09-12 15:34:35 +0900

    deviseのビューファイルの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,5 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :family_name_kana, :first_name, :first_name_kana, :birth])
-　end
+  end
 end
