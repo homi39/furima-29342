73e3c640a2346fee6a6bf135d57c76e6dcc62c8e
homi39 <misa.horiuchi0302@gmail.com>
2020-09-15 11:38:17 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   with_options presence: true do
     validates :nickname
-    validates :email, uniqueness: true, format: { with: /@.+/ }
+    validates :email, uniqueness: true
     validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
     validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}, length: { minimum: 6 }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
     validates :birth
   end
   has_many :items
