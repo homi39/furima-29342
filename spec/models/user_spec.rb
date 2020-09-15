f90a230f9b5e5d2a2b9beb0efebfbbfbbcd637f9
homi39 <misa.horiuchi0302@gmail.com>
2020-09-15 11:49:45 +0900

    テストケースの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -65,9 +65,15 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
-      it 'パスワードが半角英数字混合でないと登録できない' do
+      it 'パスワードが数字のみでは登録できない' do
         @user.password = '000000'
         @user.password_confirmation = '000000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid')
+      end
+      it 'パスワードが英字のみでは登録できない' do
+        @user.password = 'aaaaaa'
+        @user.password_confirmation = 'aaaaaa'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is invalid')
       end
