eb778f3cb9cdc466e56b1337702f6a16e69c366b
homi39 <misa.horiuchi0302@gmail.com>
2020-09-19 21:15:38 +0900

    購入ページの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,32 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :move_to_index
+
   def index
+    @order = Order.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @item = Item.create(params[:item_id])
+    @order = @item.order.new(price: order_params[:price])
+    if @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.permit(:price, :token)
+  end
+  
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 end
