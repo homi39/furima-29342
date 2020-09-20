a2271f64bb450e53aa8be4ce3aebbc4522c5320a
homi39 <misa.horiuchi0302@gmail.com>
2020-09-20 11:53:29 +0900

    購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,9 +9,11 @@ class OrdersController < ApplicationController
 
   def create
     @item = Item.create(params[:item_id])
-    @order = @item.order.new(price: order_params[:price])
-    if @order.save
-      redirect_to root_path
+    @order = @item.order.new(order_params)
+    if @order.valid?
+      pay_item 
+      @order.save
+      return redirect_to root_path
     else
       render :index
     end
@@ -20,7 +22,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.permit(:price, :token)
+    params.permit(:price, :token).merge(user_id: current_user.id, item_id: params[:item_id])
   end
   
   def move_to_index
@@ -28,5 +30,14 @@ class OrdersController < ApplicationController
     if current_user.id == @item.user_id
       redirect_to root_path
     end
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency:'jpy'
+    )
   end
 end
